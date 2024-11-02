59f6684a74c3e86fcbfa94a3b380629ded72c2da
Miguel <lmbl@ecomp.poli.br>
2024-11-01 22:07:51 -0300

    Implementação de View de Conteúdos

--- resources/views/home/contents/index.blade.php
+++ resources/views/home/contents/index.blade.php
@@ -6,59 +6,39 @@
     <div class="row g-5">
         <h1 class="fs-1 mb-3 text-center">Conteúdo sobre bem-estar</h1>
 
-        <div class="col-lg-3 col-sm-6">
-            <div class="card">
-                <img src="/images/artigo.png" class="card-img-top" alt="Artigo">
-                <div class="card-body">
-                    <h5 class="card-title mb-3">Card title</h5>
-                    <a href="#" class="btn btn-primary" target="_blank">Acessar</a>
+        @foreach($contents as $content)
+            <div class="col-lg-3 col-sm-6">
+                <div class="card">
+                    {{-- Seleciona a imagem com base no content_type_id --}}
+                    @php
+                        $imagePath = '';
+                        switch ($content->content_type_id) {
+                            case 1:
+                                $imagePath = asset('images/artigo.png');
+                                break;
+                            case 2:
+                                $imagePath = asset('images/video.png');
+                                break;
+                            case 3:
+                                $imagePath = asset('images/podcast.png');
+                                break;
+                            default:
+                                $imagePath = asset('images/default.png'); // Imagem padrão, caso precise
+                        }
+                    @endphp
+                    <img src="{{ $imagePath }}" class="card-img-top" alt="{{ $content->title }}">
+                    <div class="card-body">
+                        <h5 class="card-title mb-3">{{ $content->title }}</h5>
+                        {{-- Link dinâmico para o conteúdo --}}
+                        <a href="{{ $content->link }}" class="btn btn-primary" target="_blank">Acessar</a>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="col-lg-3 col-sm-6">
-            <div class="card">
-                <img src="/images/podcast.png" class="card-img-top" alt="Artigo">
-                <div class="card-body">
-                    <h5 class="card-title mb-3">Card title</h5>
-                    <a href="#" class="btn btn-primary" target="_blank">Acessar</a>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-3 col-sm-6">
-            <div class="card">
-                <img src="/images/video.png" class="card-img-top" alt="Artigo">
-                <div class="card-body">
-                    <h5 class="card-title mb-3">Card title</h5>
-                    <a href="#" class="btn btn-primary" target="_blank">Acessar</a>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-3 col-sm-6">
-            <div class="card">
-                <img src="/images/video.png" class="card-img-top" alt="Artigo">
-                <div class="card-body">
-                    <h5 class="card-title mb-3">Card title</h5>
-                    <a href="#" class="btn btn-primary" target="_blank">Acessar</a>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-3 col-sm-6">
-            <div class="card">
-                <img src="/images/video.png" class="card-img-top" alt="Artigo">
-                <div class="card-body">
-                    <h5 class="card-title mb-3">Card title</h5>
-                    <a href="#" class="btn btn-primary" target="_blank">Acessar</a>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-3 col-sm-6">
-            <div class="card">
-                <img src="/images/video.png" class="card-img-top" alt="Artigo">
-                <div class="card-body">
-                    <h5 class="card-title mb-3">Card title</h5>
-                    <a href="#" class="btn btn-primary" target="_blank">Acessar</a>
-                </div>
-            </div>
-        </div>
+        @endforeach
+    </div>
+
+    {{-- Paginação --}}
+    <div class="d-flex justify-content-center">
+        {{ $contents->links() }}
     </div>
 @endsection
