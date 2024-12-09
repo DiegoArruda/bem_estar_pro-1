93f5095c5f23c668861dfbdb6cdee789e2384403
Edemilton Alcides Galindo Junior <edemiltonjr@gmail.com>
2024-12-09 02:20:10 -0300

    Correção de erro no título das páginas de histórico de avaliações

--- resources/views/admin/employees/show.blade.php
+++ resources/views/admin/employees/show.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'BemEstar Pro - Funcionários')
 
 @section('content')
-    <h1 class="fs-2 mb-4">Histório de funcionário</h1>
+    <h1 class="fs-2 mb-4">Histórico de funcionário</h1>
 
     <h2 class="fs-5 mb-4">{{ $employee->name }}</h2>
 
@@ -68,7 +68,7 @@
         });
     </script>
 
-    <h2 class="fs-4 mb-4">Histório de avaliações</h2>
+    <h2 class="fs-4 mb-4">Histórico de avaliações</h2>
 
     <section class="historico-avaliacoes mb-5 bg-light p-3 rounded-4">
         <table class="table">
