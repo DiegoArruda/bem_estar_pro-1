3f9448a7f62b43e036f153d7905fd455600016f7
Edemilton Alcides Galindo Junior <edemiltonjr@gmail.com>
2024-12-20 22:46:44 -0300

    Criação de arquivo de insert de registros na tabela de Questions e Options

---
+++ database/inserts.sql
@@ -0,0 +1,18 @@
+/* INSERT DE QUESTÕES PARA AVALIAÇÃO */
+INSERT INTO `questions` VALUES (1,'Como você avalia o seu nível de bem-estar com a equipe?','on','2024-10-20 06:17:02','2024-10-20 06:17:02');
+INSERT INTO `questions` VALUES (2,'Como você avalia o seu nível de bem-estar com seu superior imediato?','on','2024-10-20 06:17:11','2024-10-20 06:17:11');
+INSERT INTO `questions` VALUES (3,'Como você classifica o ambiente de trabalho em termos de conforto e segurança?','off','2024-10-20 06:17:20','2024-11-04 05:54:50');
+INSERT INTO `questions` VALUES (4,'Como você se sente em relação ao volume de trabalho atualmente?','off','2024-10-20 06:17:30','2024-11-04 05:54:45');
+INSERT INTO `questions` VALUES (5,'Como você se sente em relação à carga emocional do seu trabalho diário?','off','2024-10-20 06:17:37','2024-11-04 05:52:55');
+INSERT INTO `questions` VALUES (6,'Como você classifica o suporte que recebe em momentos de pressão ou prazos apertados?','off','2024-10-20 06:17:47','2024-11-04 05:52:43');
+INSERT INTO `questions` VALUES (7,'Como você avalia seu estado emocional ao final do expediente?','on','2024-10-20 06:17:56','2024-10-20 06:17:56');
+INSERT INTO `questions` VALUES (8,'Como você avalia o impacto do ambiente de trabalho na sua saúde mental?','on','2024-10-20 06:18:13','2024-11-01 05:50:57');
+INSERT INTO `questions` VALUES (9,'Qual é a sua percepção sobre o equilíbrio entre vida pessoal e profissional?','on','2024-10-20 06:18:25','2024-10-20 06:18:25');
+INSERT INTO `questions` VALUES (10,'Como você avalia o seu estado emocional atualmente?','on','2024-10-20 06:18:33','2024-10-20 06:18:33');
+
+/* INSERT DE OPÇÕES DE RESPOSTA */
+INSERT INTO `options` VALUES (1,'Muito Insatisfeito',1,NULL,NULL);
+INSERT INTO `options` VALUES (2,'Insatisfeito',2,NULL,NULL);
+INSERT INTO `options` VALUES (3,'Neutro',3,NULL,NULL);
+INSERT INTO `options` VALUES (4,'Satisfeito',4,NULL,NULL);
+INSERT INTO `options` VALUES (5,'Muito Satisfeito',5,NULL,NULL);
